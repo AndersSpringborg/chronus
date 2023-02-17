50999761ee044d18ff9b9848bd144d494758d408
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-02-17 11:32:47 +0100

    :tada: Initial commit

---
+++ chronus/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""A energy scheduling model, build for HPC."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
