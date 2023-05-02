d56cf8d3ea9c33ac0dfc375359914c85fb50e456
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-05-02 11:02:10 +0200

    sqlite

--- chronus/SystemIntegration/sqlite_repository.py
+++ chronus/SystemIntegration/sqlite_repository.py
@@ -1,11 +1,12 @@
 import logging
 import sqlite3
-from datetime import datetime
 
 from chronus.domain.interfaces.benchmark_run_repository_interface import (
     BenchmarkRunRepositoryInterface,
 )
 from chronus.domain.Run import Run
+from chronus.domain.system_sample import SystemSample
+from tests.fixtures import datetime_from_string
 
 CREATE_TABLE_QUERY = """
 CREATE TABLE IF NOT EXISTS runs (
@@ -38,6 +39,27 @@ INSERT INTO runs (
 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 """
 
+CREATE_SYSTEM_SAMPLES_TABLE_QUERY = """
+CREATE TABLE IF NOT EXISTS system_samples (
+    id INTEGER PRIMARY KEY,
+    run_id INTEGER,
+    timestamp TEXT,
+    current_power_draw REAL,
+    FOREIGN KEY(run_id) REFERENCES runs(id)
+);
+"""
+
+INSERT_SYSTEM_SAMPLE_QUERY = """
+INSERT INTO system_samples (
+    run_id,
+    timestamp,
+    current_power_draw
+) VALUES (?, ?, ?);
+"""
+
+GET_ALL_SYSTEM_SAMPLES_QUERY = "SELECT * FROM system_samples WHERE run_id = ?;"
+
+
 GET_ALL_RUNS_QUERY = "SELECT * FROM runs;"
 
 
@@ -53,7 +75,7 @@ class SqliteRepository(BenchmarkRunRepositoryInterface):
             cursor = conn.cursor()
             for row in cursor.execute(GET_ALL_RUNS_QUERY):
                 (
-                    _,
+                    run_id,
                     cpu,
                     cores,
                     thread_per_core,
@@ -75,6 +97,7 @@ class SqliteRepository(BenchmarkRunRepositoryInterface):
                 run.flop = float(flop)
                 run._energy_used_joules = float(energy_used)
                 run._gflops_per_watt = float(gflops_per_watt)
+                run._samples = self._get_system_samples(run_id=run_id)
                 runs.append(run)
         return runs
 
@@ -96,6 +119,13 @@ class SqliteRepository(BenchmarkRunRepositoryInterface):
                     run.end_time,
                 ),
             )
+            run_id = cursor.lastrowid
+
+            for sample in run._samples:
+                cursor.execute(
+                    INSERT_SYSTEM_SAMPLE_QUERY,
+                    (run_id, sample.timestamp, sample.current_power_draw),
+                )
             conn.commit()
         self.logger.info(f"Run data has been saved to {self.path}.")
 
@@ -103,4 +133,17 @@ class SqliteRepository(BenchmarkRunRepositoryInterface):
         with sqlite3.connect(self.path) as conn:
             cursor = conn.cursor()
             cursor.execute(CREATE_TABLE_QUERY)
-        self.logger.info(f"Table 'runs' has been created in {self.path}.")
+            cursor.execute(CREATE_SYSTEM_SAMPLES_TABLE_QUERY)
+        self.logger.info(f"Tables 'runs' and 'system_samples' have been created in {self.path}.")
+
+    def _get_system_samples(self, run_id: int) -> list[SystemSample]:
+        samples = []
+        with sqlite3.connect(self.path) as conn:
+            cursor = conn.cursor()
+            for row in cursor.execute(GET_ALL_SYSTEM_SAMPLES_QUERY, (run_id,)):
+                _, _, timestamp, current_power_draw = row
+                sample = SystemSample(
+                    timestamp=datetime_from_string(timestamp), current_power_draw=current_power_draw
+                )
+                samples.append(sample)
+        return samples
