23fad99c4fb0f1ccd8b9255c23e4cf5ee938f3a5
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-05-09 12:40:57 +0200

    system service is getting cpu freq

--- chronus/SystemIntegration/system_service_interfaces/ipmi_system_service.py
+++ chronus/SystemIntegration/system_service_interfaces/ipmi_system_service.py
@@ -1,12 +1,13 @@
 import datetime
 from dataclasses import dataclass
 
+import psutil
 import pyghmi.ipmi.command as ipmi
 from dataclasses_json import dataclass_json
 from pyghmi.ipmi.sdr import SensorReading
 
 from chronus.domain.interfaces.system_service_interface import SystemServiceInterface
-from chronus.domain.system_sample import SystemSample
+from chronus.domain.system_sample import CpuFreq, SystemSample
 
 
 @dataclass
@@ -22,6 +23,10 @@ class SensorData:
     health: int
 
 
+def _get_cpu_freq() -> [CpuFreq]:
+    return psutil.cpu_freq(percpu=True)
+
+
 class IpmiSystemService(SystemServiceInterface):
     _conn: ipmi.Command
 
@@ -32,12 +37,14 @@ class IpmiSystemService(SystemServiceInterface):
         current_power_draw = self._get_system_power_draw()
         cpu_temp = self._get_cpu_temp()
         cpu_power = self._get_cpu_power()
+        cpu_freq = _get_cpu_freq()
 
         return SystemSample(
             datetime.datetime.now(),
             current_power_draw=current_power_draw,
             cpu_temp=cpu_temp,
             cpu_power=cpu_power,
+            cpu_freq=cpu_freq,
         )
 
     def _get_system_power_draw(self) -> float:
--- chronus/domain/system_sample.py
+++ chronus/domain/system_sample.py
@@ -1,7 +1,13 @@
+from typing import TypedDict
+
 import datetime
 from dataclasses import dataclass
 
-from psutil._common import scpufreq
+
+class CpuFreq(TypedDict):
+    current: float
+    min: float
+    max: float
 
 
 @dataclass
@@ -10,4 +16,4 @@ class SystemSample:
     current_power_draw: float = 1.0
     cpu_power: float = 0.0
     cpu_temp: float = 0.0
-    cpu_freq: [scpufreq] = None
+    cpu_freq: [CpuFreq] = None
